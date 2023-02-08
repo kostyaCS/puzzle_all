e9ee1aebfbed93b25b17b67ad6e02240b427123f
kostyaCS <116595519+kostyaCS@users.noreply.github.com>
2023-02-08 13:17:35 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -3,7 +3,10 @@ def check_horizontal(board: list, row_num: int) -> bool:
     Function checks if there are no same digit numbers
     in one row on the board.
     """
-    pass
+    for digit in board[row_num]:
+        if digit.isdigit() and board[row_num].count(digit) > 1:
+            return False
+    return True
 
 
 def check_vertical(board: list, index: int) -> bool:
@@ -29,13 +32,23 @@ def check_by_flag(board: list) -> bool:
     Function checks if there are no same digit numbers
     in one flag on the board.
     """
-    pass
+    for index_i in range(9):
+        test = board[8 - index_i][index_i+1:]
+        for index_j in range(0, 9-index_i):
+            test += board[index_j][index_i]
+        for check in range(1, 10):
+            if test.count(str(check)) >= 2:
+                return False
+    return True
 
 
 def validate_board(board):
-
+    """
+    Validates all the board
+    """
     for i in range(9):
         if not (check_horizontal(board, i) and check_vertical(board, i)):
             return False
-
-    return check_by_flag(board)
+    if not check_by_flag(board):
+        return False
+    return True
